3ff23d838f7dec505b592064d0757febe5d6362d
zhangliyuan <1019255652@qq.com>
2019-01-14 19:26:17 +0800

    No.1

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -11,12 +11,12 @@ export default class Login extends Component {
     return (
       <div className="login">
         <div className="login-header">
-          <image src={logo} alt="logo"/>
+          <img src={logo} alt="logo"/>
           React项目：后台管理系统
         </div>
         <div className="login-content">
           <div className="login-box">
-            <div className="title">用户登录</div>
+            <div className="title">用户登陆</div>
             <LoginForm/>
           </div>
         </div>
@@ -26,16 +26,34 @@ export default class Login extends Component {
 }
 // 包含<Form>被包装组件
 class LoginForm extends React.Component{
-    checkPassword = () => {
-
+    checkPassword = (rule,value,callback) => {
+        if(!value){
+          callback('必须输入密码')
+        }else if(value.length<4 || value.length>8){
+          callback('密码必须是4到8位')
+        }else {
+          callback()
+        }
+    }
+    clickLogin =() => {
+        this.props.form.validateFields((error,values) =>{
+            if(!error){
+                console.log('收集表单数据',values)
+            }else {
+               this.props.from.resetFields()
+            }
+        })
     }
   render(){
+
     const {getFieldDecorator}=this.props.form
+
     return(
        <Form className="login-form">
           <Form.Item>
               {
                 getFieldDecorator('username',{
+                  initialValue:'5211314',
                   rules:[
                       {type:'string',required:true,message:'必须输入用户名'},
                       {min:4,message:'长度不得少于4'}
@@ -51,7 +69,7 @@ class LoginForm extends React.Component{
               }
           </Form.Item>
           <Form.Item>
-            <Button type="primary" className="login-form-button">登陆</Button>
+            <Button type="primary" className="login-form-button" onClick={this.clickLogin}>登陆</Button>
           </Form.Item>
        </Form>
     )
